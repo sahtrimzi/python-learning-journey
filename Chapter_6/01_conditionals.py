b6b30c020d9ddf34766e8900f4da85d9d078b6ed
Syed Ali Haider Trimzi <syedalihaidertrimzi01@gmail.com>
2026-09-22 21:53:56 +0500

    Chapter 6 if elif and else

---
+++ Chapter_6/01_conditionals.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age: "))
+
+if(a>18): #condtion if age is greater than a then print below line 
+    print("Your are above the age of concent")
+
+else:  #if the first condition then print else statement 
+    print("Your are below the age of concent")
+
+
+
+    
---
+++ Chapter_6/02_if_elif_ladder.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your age: "))
+
+if(a>18): 
+    print("Your are above the age of concent")
+
+elif(a<0): #in program we need multuple conditions is required then we use elif
+    print("You are enterning the invalid age ")
+
+else:
+    print("Your are below the age of concent")
+
+
+
+    
